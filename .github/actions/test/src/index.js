ba7c6ad29eb843eac2b531a22317528a8ff8ecd3
dheerajmogaveera <47212866+dheerajmogaveera@users.noreply.github.com>
2023-10-18 13:38:17 +0530

    test

--- .github/actions/test/src/index.js
+++ .github/actions/test/src/index.js
@@ -2,8 +2,10 @@ const core = require('@actions/core');
 
 async function run() {
     const secret = core.getInput('value', { required: true });
-    console.log("env secret:",secret);
-    const json = JSON.parse(secret);
+    let bufferObj = Buffer.from(secret, "base64"); 
+    let decodedString = bufferObj.toString("utf8"); 
+    console.log("env secret:",decodedString);
+    const json = JSON.parse(decodedString);
     console.log("json:",json);
     core.setOutput('secret', json);
 }
