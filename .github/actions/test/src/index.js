260c2142cd47f6f4c31f3fef2a66985828964fe4
dheerajmogaveera <47212866+dheerajmogaveera@users.noreply.github.com>
2023-10-18 13:17:34 +0530

    test

--- .github/actions/test/src/index.js
+++ .github/actions/test/src/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 
 async function run() {
     const secret = process.env.MY_JSON_SECRET;
-    console.log("env:",secret);
+    console.log("env:",process.env);
     const json = JSON.parse(secret);
     console.log("json:",json);
     core.setOutput('secret', json);
