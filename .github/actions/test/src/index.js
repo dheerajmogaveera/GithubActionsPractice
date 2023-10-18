b2b40c725a382d824df0052c0220fc9c66ed87ae
dheerajmogaveera <47212866+dheerajmogaveera@users.noreply.github.com>
2023-10-18 13:33:47 +0530

    test

--- .github/actions/test/src/index.js
+++ .github/actions/test/src/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 
 async function run() {
     const secret = core.getInput('value', { required: true });
-    console.log("env:",secret);
+    console.log("env secret:",secret);
     const json = JSON.parse(secret);
     console.log("json:",json);
     core.setOutput('secret', json);
