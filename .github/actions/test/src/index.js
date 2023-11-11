bb267a12889604dc542cab53243aa3c3f08309f7
dheerajmogaveera <47212866+dheerajmogaveera@users.noreply.github.com>
2023-11-11 12:28:30 +0530

    Update index.js

--- .github/actions/test/src/index.js
+++ .github/actions/test/src/index.js
@@ -2,6 +2,7 @@ import core from '@actions/core';
 import test from '../../../../helper/helper.js';
 async function run() {
     const json = test();
+    //testing
     core.setOutput('secret', json);
 }
 
