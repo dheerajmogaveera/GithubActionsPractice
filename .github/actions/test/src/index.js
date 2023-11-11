e1e9afb8317f0be892632a16679ec2de984c1c0b
dheerajmogaveera <47212866+dheerajmogaveera@users.noreply.github.com>
2023-11-11 12:09:18 +0530

    test

--- .github/actions/test/src/index.js
+++ .github/actions/test/src/index.js
@@ -1,12 +1,14 @@
 import core from '@actions/core';
 async function run() {
     //testing multiple commits to branch
+
     const secret = process.env.MY_JSON_SECRET;
     let bufferObj = Buffer.from(secret, "base64");
     let decodedString = bufferObj.toString("utf8");
     console.log("env secret:", decodedString);
     const json = JSON.parse(decodedString);
     console.log("json:", json);
+
     core.setOutput('secret', json);
 
 }
